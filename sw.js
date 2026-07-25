39b4ffdbf7271f78cd24a02e2521797a56939554
Oleksandr <oleksandr.kiris@icloud.com>
2026-07-25 07:47:28 +0200

    Focus mobile application workspace

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const CACHE_PREFIX = "citronex-jobs-";
-const CACHE_VERSION = "citronex-jobs-v131-vacancy-detail-flow-2026-07-25";
+const CACHE_VERSION = "citronex-jobs-v132-focused-application-workspace-2026-07-25";
 const CORE_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./data/content.js?v=131",
-  "./data/locales/ru.js?v=131",
-  "./data/locales/en.js?v=131",
-  "./assets/styles.css?v=131",
-  "./assets/i18n.js?v=131",
-  "./assets/application-form.js?v=131",
-  "./assets/app.js?v=131",
+  "./data/content.js?v=132",
+  "./data/locales/ru.js?v=132",
+  "./data/locales/en.js?v=132",
+  "./assets/styles.css?v=132",
+  "./assets/i18n.js?v=132",
+  "./assets/application-form.js?v=132",
+  "./assets/app.js?v=132",
   "./assets/icons.svg",
   "./assets/mobility-map.svg?v=10",
   "./assets/citronex-logo.jpg",
@@ -28,18 +28,18 @@ const CORE_SHELL = [
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/share-card.svg",
-  "./assets/share-card-v11.png?v=131"
+  "./assets/share-card-v11.png?v=132"
 ];
 const OPTIONAL_LOCALES = [
-  "./data/locales/uk.js?v=131",
-  "./data/locales/pl.js?v=131",
-  "./data/locales/az.js?v=131",
-  "./data/locales/ka.js?v=131",
-  "./data/locales/id.js?v=131",
-  "./data/locales/es.js?v=131",
-  "./data/locales/fil.js?v=131",
-  "./data/locales/ne.js?v=131",
-  "./data/locales/hy.js?v=131"
+  "./data/locales/uk.js?v=132",
+  "./data/locales/pl.js?v=132",
+  "./data/locales/az.js?v=132",
+  "./data/locales/ka.js?v=132",
+  "./data/locales/id.js?v=132",
+  "./data/locales/es.js?v=132",
+  "./data/locales/fil.js?v=132",
+  "./data/locales/ne.js?v=132",
+  "./data/locales/hy.js?v=132"
 ];
 
 self.addEventListener("install", (event) => {
