f0c46f2497d9273f267f8e18bba273af71636f65
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-28 12:27:30 +0200

    Refine application form CSS (v193)

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 const CACHE_PREFIX = "kiris-jobs-";
-const CACHE_VERSION = "kiris-jobs-v192-recruiter-message-2026-07-28";
+const CACHE_VERSION = "kiris-jobs-v193-application-css-2026-07-28";
 const CORE_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./data/content.js?v=192",
-  "./data/locales/ru.js?v=192",
-  "./data/locales/en.js?v=192",
-  "./assets/styles.css?v=192",
-  "./assets/clean.css?v=192",
-  "./assets/i18n.js?v=192",
-  "./assets/application-form.js?v=192",
-  "./assets/candidate.js?v=192",
+  "./data/content.js?v=193",
+  "./data/locales/ru.js?v=193",
+  "./data/locales/en.js?v=193",
+  "./assets/styles.css?v=193",
+  "./assets/clean.css?v=193",
+  "./assets/i18n.js?v=193",
+  "./assets/application-form.js?v=193",
+  "./assets/candidate.js?v=193",
   "./assets/icons.svg",
   "./assets/mobility-map.svg?v=10",
   "./assets/oleksandr-kiris-greenhouse.jpg",
@@ -28,18 +28,18 @@ const CORE_SHELL = [
   "./assets/icon-192.png",
   "./assets/icon-512.png",
   "./assets/share-card.svg",
-  "./assets/share-card.png?v=192"
+  "./assets/share-card.png?v=193"
 ];
 const OPTIONAL_LOCALES = [
-  "./data/locales/uk.js?v=192",
-  "./data/locales/pl.js?v=192",
-  "./data/locales/az.js?v=192",
-  "./data/locales/ka.js?v=192",
-  "./data/locales/id.js?v=192",
-  "./data/locales/es.js?v=192",
-  "./data/locales/fil.js?v=192",
-  "./data/locales/ne.js?v=192",
-  "./data/locales/hy.js?v=192"
+  "./data/locales/uk.js?v=193",
+  "./data/locales/pl.js?v=193",
+  "./data/locales/az.js?v=193",
+  "./data/locales/ka.js?v=193",
+  "./data/locales/id.js?v=193",
+  "./data/locales/es.js?v=193",
+  "./data/locales/fil.js?v=193",
+  "./data/locales/ne.js?v=193",
+  "./data/locales/hy.js?v=193"
 ];
 
 self.addEventListener("install", (event) => {
